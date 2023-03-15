42fa5069e4ff45c4197fe57c739fd55c48147eda
Hwang Jina <wlsk401@gmail.com>
2023-03-15 17:51:42 +0900

    create global style

--- src/MobileView/routes/Mhome.js
+++ src/MobileView/routes/Mhome.js
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+`;
+const Logo = styled.h1`
+  font-size: 18px;
+  color: yellow;
+  font-weight: 900;
+`;
 
 function Mhome() {
-  return <div></div>;
+  return (
+    <Header>
+      <Logo>BANANA</Logo>
+    </Header>
+  );
 }
 export default Mhome;
