6ed135109b9183328af16551bd917f3ede53cbfa
Hwang Jina <wlsk401@gmail.com>
2023-03-15 17:25:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import { BrowserView, MobileView } from "react-device-detect";
-import { BrowserRouter, Routes, Route, Link, Switch } from "react-router-dom";
-import Desktopnext from "./desktopnext";
-import MovileNext from "./movilenext";
 import { DeskTopRouter, MobileRouter } from "./Router";
 
 function App() {
