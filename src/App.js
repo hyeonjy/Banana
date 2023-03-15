66ccbe87268eeeef39db56c7f455201af339571b
hyeonjy <hg024246@gmail.com>
2023-03-15 15:25:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { BrowserView, MobileView } from "react-device-detect";
 
 function App() {
-  return <div>s</div>;
+  return (
+    <div>
+      <BrowserView>데스크톱브라우져!</BrowserView>
+      <MobileView>모바일 브라우져!</MobileView>
+    </div>
+  );
 }
 
 export default App;
