c708bfc956564e739c0b6888286486af732f051a
hyeonjy <hg024246@gmail.com>
2023-03-15 15:53:53 +0900

    install router-dom and styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserView, MobileView } from "react-device-detect";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <BrowserView>데스크톱브라우져!</BrowserView>
       <MobileView>모바일 브라우져~!</MobileView>
-    </div>
+    </BrowserRouter>
   );
 }
 
