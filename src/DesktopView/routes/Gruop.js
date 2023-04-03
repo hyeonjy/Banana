6907ec4bb46413ecb889d40af7bfa93238806982
Hwang Jina <wlsk401@gmail.com>
2023-04-03 18:33:24 +0900

    Update Gruop.js

--- src/DesktopView/routes/Gruop.js
+++ src/DesktopView/routes/Gruop.js
@@ -1,369 +1,226 @@
-import { useEffect, useRef, useState } from "react";
-import styled, { css } from "styled-components";
+import { useLocation, useParams } from "react-router";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "../components/Nav";
+import NoItem from "../components/NoItem";
+import SideNav from "../components/SideNav";
 import { itemsGroup } from "../ItemGroup";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faImage } from "@fortawesome/free-regular-svg-icons";
-import { BackGround } from "./Gruop";
-import { faX } from "@fortawesome/free-solid-svg-icons";
-import area from "../components/Area";
+import { ProductList } from "../ItemObject";
+import { Container } from "./Home";
 
-const Container = styled.div`
-  margin: 157px auto;
-  flex-direction: column;
-  align-items: space-evenly;
-  display: flex;
-  width: 900px; /**900 */
-  z-index: 1;
-  position: relative;
-  background-color: white;
-  border-radius: 21px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 25px 100px -20px,
-    rgba(0, 0, 0, 0.3) 0px 25px 60px -30px;
-  margin-bottom: 100px;
-  padding: 50px;
+export const CateContainer = styled(Container)`
+  width: 950px;
+  //margin: 0 auto;
+  padding-top: 0;
+  padding-bottom: 70px;
 `;
 
-// 게시글 (메인 글씨)
-const KeySubject = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  border-bottom: 1px solid #e9ecef;
-  padding: 10px;
-  width: 850px; /**850 */
-  margin-left: 10px;
-`;
-
-// 폼 전체
-const CreateForm = styled.form`
-  padding: 10px 20px;
-`;
-
-// space-between을 위한 column Box
-const Box = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 850px; /**850 */
-  margin: 10px 0;
-  ${(props) =>
-    props.primary &&
-    css`
-      padding-bottom: 10px;
-      border-bottom: 1px solid #e9ecef;
-    `}
-`;
-
-// 카테고리 or 지역의 select를 합친 Box
-const CategoryBox = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-// 카테고리, 지역 title
-const SelectTitle = styled.h1`
-  font-size: 20px;
-  margin-right: 5px;
-  color: gray;
-`;
-
-const Select = styled.select.attrs({ required: true })`
-  height: 30px;
-  width: 100px;
-  border-radius: 8px;
-  margin-left: 5px;
-`;
-
-const Option = styled.option`
-  font-size: 15px;
-`;
-
-// 제목과 내용 input
-const TextInput = styled.textarea.attrs({ required: true })`
-  font-size: 15px;
-  padding: 10px;
-  border: 1px solid rgba(0, 0, 0, 0.3);
-  width: 830px; /**830 */
-  resize: none;
-  border-radius: 5px;
-  height: 150px;
-  &:focus {
-    outline-color: rgb(255, 230, 0);
-  }
-  ${(props) =>
-    props.titletext &&
-    css`
-      line-height: 28px;
-      height: 30px;
-      margin: 10px 0;
-    `}
-`;
-
-const CameraLabel = styled.label`
-  width: 50px;
+//--------상위 카테고리-------//
+export const TopCate = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  background-color: #f9f9fa;
-  padding: 5px 2px;
-  border-radius: 8px;
-  font-size: 13px;
+  padding: 20px 0 30px;
+  border-bottom: 3px solid rgb(167 167 167 / 28%);
 `;
-
-const CameraIcon = styled(FontAwesomeIcon)`
-  font-size: 25px;
-  margin-bottom: 3px;
-`;
-
-const ImgInput = styled.input.attrs({ required: true })`
-  opacity: 0;
-  position: absolute;
-  left: 60px;
-`;
-
-// 미리보기 이미지 전체 div
-const PreviewBox = styled.div`
-  display: flex;
-  overflow-x: auto;
-
-  &::-webkit-scrollbar {
-    height: 8px; /* 스크롤바의 너비 */
-  }
-  &::-webkit-scrollbar-thumb {
-    height: 12px; /* 스크롤바의 길이 */
-    background: orange; /* 스크롤바의 색상 */
-    border-radius: 10px;
-  }
-  &::-webkit-scrollbar-track {
-    background-color: rgba(121, 121, 121, 0.3); /*스크롤바 뒷 배경 색상*/
-    border-radius: 12px;
-  }
-`;
-
-const ImgBox = styled.div`
-  position: relative;
-  span {
-    position: absolute;
-    top: 13px;
-    left: 3px;
-    background-color: green;
-    padding: 5px 10px;
-    color: white;
-    border-radius: 5px;
-  }
-`;
-
-const ImgPreview = styled.img`
-  width: 250px;
-  height: 250px;
-  object-fit: cover;
-  margin: 10px;
-  margin-left: 0;
-`;
-
-// submit 버튼
-const SubmitBtn = styled.button`
-  font-size: 16px;
-  padding: 3px 12px;
-  border-radius: 6px;
-  line-height: 32px;
-  border-radius: 1px solid tomato;
-  background-color: rgb(255, 230, 0);
-  border: none;
+export const CategoryMain = styled.span`
+  font-size: 26px;
   font-weight: 700;
+  line-height: 85px;
+  width: 50%;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+export const CategoryUl = styled.ul`
+  display: flex;
+  width: 60%;
+  position: relative;
+  line-height: 30px;
+`;
+export const Categoryli = styled(Link)`
+  //flex-grow: 1;
+  width: 20%;
+  text-align: center;
+  cursor: pointer;
+
+  z-index: 20;
+  &:hover {
+    font-weight: 800;
+  }
+`;
+
+//------------Item Query-----------//
+export const QueryDiv = styled.div``;
+
+export const QueryUl = styled.ul`
+  width: 30%;
+  height: 40px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 10px 0;
+`;
+export const QueryLi = styled.li`
+  font-size: 14px;
+  padding-left: 10px;
+  &:not(:first-of-type) {
+    border-left: 1px solid gray;
+    padding-left: 20px;
+    margin-left: 20px;
+  }
+  &:hover {
+    font-weight: 700;
+  }
   cursor: pointer;
 `;
 
-const Xbtn = styled(FontAwesomeIcon)`
-  font-size: 12px;
-  padding: 9px;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  z-index: 5;
-  color: white;
-  background: black;
+//--------Item List-------//
+export const ItemDiv = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  min-height: 60vh;
+  padding-left: 10px;
+`;
+export const Product = styled(Link)`
+  &:not(:nth-of-type(4n + 1)) {
+    margin-left: 4%;
+  }
+  margin-bottom: 30px;
+  width: 21%;
+`;
+export const ProductImg = styled.img`
+  //width: 250px;
+  height: 200px;
+  width: 100%;
+  object-fit: cover;
+  margin-bottom: 10px;
+`;
+export const ProductTitle = styled.h1`
+  font-weight: 600;
+  font-size: 16px;
+
+  overflow: hidden; // 을 사용해 영역을 감출 것
+  text-overflow: ellipsis; // 로 ... 을 만들기
+  white-space: nowrap; // 아래줄로 내려가는 것을 막기위해
+  word-break: break-all;
+  margin-bottom: 5px;
+`;
+export const ProductDetail = styled.span`
+  font-size: 13px;
+  color: #232323ab;
 `;
 
-function Upload() {
-  const [minor, setMinor] = useState(["선택하세요"]); /**옷 소분류 */
-  const [imgFile, setImgFile] = useState([]); /**이미지 파일 */
-  const imgRef = useRef();
-  const [object, setObject] = useState({}); /**제출시 등록되는 item-object */
+//배너?
+export const BackGround = styled.div`
+  position: absolute;
+  top: 0px;
+  z-index: 0;
+  min-width: 1300px;
+  width: 100%;
+  //height: 293px;
+  //background-color: beige;
+  height: 549px;
+  background: linear-gradient(360deg, rgba(255, 255, 255, 1) 0%, #ffffba 100%);
 
-  // option 값이 바뀌면 실행되는 함수
-  const optionChange = (e) => {
-    const searchItem = itemsGroup.find((item) => e.target.value === item.main);
-    if (e.target.value === "") {
-      setMinor(["선택하세요"]);
-    } else {
-      setMinor(searchItem.sub);
-    }
-  };
+  transform: translate(0, -39%);
+`;
+//----------------end-------------------//
+export const queryArray = ["최신등록순", "조회순", "관심순"];
+function Group() {
+  //url - 현재 카테고리 정보
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const categoryValue = searchParams.get("category"); // id( = main category)
+  const Group = itemsGroup.find((item) => item.id === Number(categoryValue));
 
-  // 이미지 저장 함수
-  const saveImgFile = (e) => {
-    const imageLists = e.target.files;
-    let imageUrlLists = [...imgFile];
-
-    for (let i = 0; i < imageLists.length; i++) {
-      const currentImageUrl = URL.createObjectURL(imageLists[i]);
-      imageUrlLists.push(currentImageUrl);
-    }
-
-    if (imageUrlLists.length > 5) {
-      alert("최대 5장까지만 업로드 합니다.");
-      imageUrlLists = imageUrlLists.slice(0, 5);
-    }
-    setImgFile(imageUrlLists);
-  };
-
-  // 이미지 삭제시 실행되는 함수
-  const handleDelete = (index) => {
-    setImgFile(imgFile.filter((itme, idx) => idx !== index));
-  };
-
-  // 폼 제출시 실행되는 함수 (object를 저장)
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setObject({
-      imgUrl: [...imgFile],
-      title: event.target.title.value,
-      detail: event.target.area.value,
-      view: "0",
-      main: event.target.major.value,
-      sub: event.target.minor.value,
-      id: "77777",
-      content: event.target.content.value,
-    });
-  };
-
-  // 이미지 썸네일 가로 스크롤
-  const previewBox = useRef();
-  useEffect(() => {
-    const el = previewBox.current;
-    const onWheel = (event) => {
-      if (el && event.deltaY !== 0) {
-        event.preventDefault();
-        el.scrollTo({
-          left: el.scrollLeft + event.deltaY,
-          behavior: "auto",
-        });
-      } else return;
-    };
-    el.addEventListener("wheel", onWheel);
-    return () => el.removeEventListener("wheel", onWheel);
-  }, []);
-
+  //현재 cate에 해당하는 item
+  const cateItem = ProductList.filter((item) => item.main === Group.main);
   return (
     <div style={{ position: "relative" }}>
-      {/* 노란색 그라데이션 배경 */}
       <BackGround />
+      <div
+        style={{
+          justifyContent: "space-evenly",
+          display: "flex",
+          width: "1300px",
+          //margin: "0 auto",
+          margin: " 157px auto",
+          zIndex: "1",
+          position: "relative",
+          backgroundColor: "white",
+          borderRadius: "21px",
+          //box-shadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset"
+          boxShadow:
+            "rgba(50, 50, 93, 0.25) 0px 25px 100px -20px, rgba(0, 0, 0, 0.3) 0px 25px 60px -30px",
+        }}
+      >
+        <SideNav />
+        <CateContainer>
+          {/* 카테고리 Link */}
+          <TopCate>
+            <CategoryMain>{Group.main}</CategoryMain>
+            <CategoryUl>
+              {Group.sub.map((sub, index) => (
+                <Categoryli
+                  to={{
+                    pathname: `/group/${categoryValue}`,
+                    search: `?subitem=${sub}`,
+                  }}
+                  key={index}
+                >
+                  {sub}
+                </Categoryli>
+              ))}
+            </CategoryUl>
+          </TopCate>
+          {/* 카테고리 끝 */}
+          {cateItem.length > 0 ? (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  margin: "8px 5px 0 5px",
+                }}
+              >
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <span style={{ fontSize: "17px", fontWeight: "600" }}>
+                    {Group.main} - 전체({cateItem.length})
+                  </span>
+                </div>
+                <QueryUl>
+                  {queryArray.map((query, index) => (
+                    <QueryLi key={index}>{query}</QueryLi>
+                  ))}
+                </QueryUl>
+              </div>
 
-      <Container>
-        <KeySubject>게시글</KeySubject>
-        {/* 폼 시작 */}
-        <CreateForm onSubmit={handleSubmit}>
-          <Box>
-            {/* 카테고리 SELECT */}
-            <CategoryBox>
-              <SelectTitle>카테고리</SelectTitle>
-              {/* 옷 대분류 */}
-              <Select name="major" onChange={optionChange}>
-                <Option value="">선택하세요</Option>
-                {itemsGroup.map((item, index) => {
-                  return (
-                    <Option key={index} value={item.main}>
-                      {item.main}
-                    </Option>
-                  );
-                })}
-              </Select>
-              {/* 옷 소분류 */}
-              <Select name="minor">
-                {minor.map((item, index) => {
-                  return (
-                    <Option key={index} value={item}>
-                      {item}
-                    </Option>
-                  );
-                })}
-              </Select>
-            </CategoryBox>
-
-            {/* 지역 SELECT */}
-            <CategoryBox>
-              <SelectTitle>지역</SelectTitle>
-              <Select name="area">
-                {area.map((item, index) => {
-                  return (
-                    <Option key={index} value={item}>
-                      {item}
-                    </Option>
-                  );
-                })}
-              </Select>
-            </CategoryBox>
-          </Box>
-
-          {/* 제목과 내용 */}
-          <TextInput
-            type="text"
-            placeholder="제목을 입력해주세요."
-            maxLength={35}
-            name="title"
-            titletext={"true"}
-          />
-          <TextInput
-            type="text"
-            placeholder="내용을 입력해주세요."
-            maxLength={255}
-            name="content"
-          />
-
-          {/* 카메라 아이콘과 등록 버튼 */}
-          <Box primary>
-            <CameraLabel>
-              <CameraIcon icon={faImage} />
-              <span>사진</span>
-            </CameraLabel>
-            <ImgInput
-              id="file"
-              type="file"
-              accept="image/*"
-              onChange={saveImgFile}
-              ref={imgRef}
-              multiple
-            />
-            <SubmitBtn type="submit">등록</SubmitBtn>
-          </Box>
-
-          {/* 사진 미리보기 */}
-          <PreviewBox ref={previewBox}>
-            {imgFile.map((item, index) => {
-              return (
-                <ImgBox key={index}>
-                  {index === 0 && <span>대표</span>}
-                  <ImgPreview
-                    src={imgFile ? item : `../../Img/banana.png`}
-                    alt="프로필 이미지"
-                  />
-                  <Xbtn
-                    onClick={(event) => {
-                      event.preventDefault();
-                      handleDelete(index);
+              <ItemDiv>
+                {cateItem.map((item, index) => (
+                  <Product
+                    key={index}
+                    to={{
+                      pathname: `/post/${item.id}`,
+                      state: {
+                        item,
+                      },
                     }}
-                    icon={faX}
-                  />
-                </ImgBox>
-              );
-            })}
-          </PreviewBox>
-        </CreateForm>
-        {/* 폼 끝 */}
-      </Container>
+                  >
+                    <ProductImg src={item.imgURL[0]} />
+                    <ProductTitle>{item.title}</ProductTitle>
+                    <ProductDetail>{item.detail}</ProductDetail>
+                  </Product>
+                ))}
+              </ItemDiv>
+            </>
+          ) : (
+            <NoItem />
+          )}
+          {/*Query*/}
+        </CateContainer>
+      </div>
     </div>
   );
 }
-
-export default Upload;
+export default Group;
