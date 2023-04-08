5f10174ca565c4f59c54e1714bab2d71d7239edb
Hwang Jina <wlsk401@gmail.com>
2023-04-08 17:54:38 +0900

    chatLi.js

--- src/DesktopView/routes/Chats.js
+++ src/DesktopView/routes/Chats.js
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 import { Container } from "./Home";
-import ChatList from "../components/ChatList";
 import Chat from "../components/Chat";
-
+import ChatLi from "../components/ChatLi";
+const ChatContainer = styled(Container)`
+  display: flex;
+  padding-top: 70px;
+  width: 80%;
+  margin: 0 auto;
+`;
 function Chats() {
   return (
-    <Container>
-      <ChatList />
+    <ChatContainer>
+      <ChatLi />
       <Chat />
-    </Container>
+    </ChatContainer>
   );
 }
 
