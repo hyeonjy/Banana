d9291f765d9ccd5620a788e6a5a0e550020916fa
Hwang Jina <wlsk401@gmail.com>
2023-04-24 17:32:09 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 
 const http = require("http").createServer(app);
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (request, response) {
+  response.sendFile(path.join(__dirname, "build/index.html"));
+});
+
 //
 app.listen(8080, function () {
   console.log("listening in 8080");
