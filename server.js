d7c3d8dbff73e32cb1d8c0474e83bb8911a94e5e
Hwang Jina <wlsk401@gmail.com>
2023-04-24 18:32:55 +0900

    env/mysql 연결

--- server.js
+++ server.js
@@ -5,12 +5,33 @@ const app = express();
 
 const http = require("http").createServer(app);
 
+require("dotenv").config();
+
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  // host: "172.30.1.84",
+  // user: "banana",
+  // password: "bananatree246",
+  // database: "mydatabase",
+  host: "172.30.1.72",
+  //port: "3306",
+  user: "banana",
+  password: process.env.DB_PASSWORD,
+  database: "mydatabase",
+});
+connection.connect();
+connection.query("SELECT * FROM mydatabase.user", (error, rows, fields) => {
+  if (error) throw error;
+  console.log("SQL: ", rows);
+});
+connection.end();
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (request, response) {
   response.sendFile(path.join(__dirname, "build/index.html"));
 });
-
+console.log(path.join(__dirname, "build/index.html"));
 //
 app.listen(8080, function () {
   console.log("listening in 8080");
