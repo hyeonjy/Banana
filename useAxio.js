63478c3208738bc3a2f99312615480ea967c33d4
Hwang Jina <wlsk401@gmail.com>
2023-04-28 13:31:35 +0900

    Create useAxio.js

---
+++ useAxio.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useAxios = ({ method, url, config }) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setloading] = useState(true);
+
+  const fetchData = () => {
+    axios[method](url, config)
+      .then((res) => {
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setloading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { response, error, loading };
+};
+
+export default useAxios;
